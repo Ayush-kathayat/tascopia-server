bf3ccd0d61c9c7f511b9e76db8a1515a9ee03c3d
Ayush-kathayat <kathayatayush789@gmail.com>
2024-06-29 20:54:47 +0530

    (init) : initialized the userRoutes folder

---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+
+//todo : import the controller
+
+//! todo :  intialize the passport
+
+
+const userRouter = express.Router();
+
+//! todo  : NOW use the controllers here like for register , login , logout etc
+
+
+
+export default userRouter;
+
